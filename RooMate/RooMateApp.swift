996ff156504c3b92162ffd9f6b6aa2fafc6c0a26
Lego Panda <legopanda565@icloud.com>
2025-10-10 21:54:35 -0400

    Initial Commit

---
+++ RooMate/RooMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  RooMateApp.swift
+//  RooMate
+//
+//  Created by Makai O’Neill on 10/10/25.
+//
+
+import SwiftUI
+
+@main
+struct RooMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
